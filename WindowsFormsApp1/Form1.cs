4ac6cb34e9662e3e2518030f15fcc4e1ad960b14
rc0029174 <rc0029174@CRO-24-12>
2019-06-03 15:23:52 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Calculate the area of a rectangle
+            try
+            {
+                double width, height, area;
+                width = Convert.ToDouble(textBox1.Text);
+                height = Convert.ToDouble(textBox2.Text);
+                area = width * height;
+                label1.Text = "Area = " + area;
+
+            }
+            catch
+            {
+                MessageBox.Show("Type in two numbers."); 
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
